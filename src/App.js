1327715872f75747e764af335e52f7e02547be3f
AnshulPatni <anshulpatni19@gmail.com>
2018-04-16 20:03:53 -0700

    Enabled react-scripts eject functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 //import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
@@ -54,18 +54,6 @@ class App extends Component {
   }
 
   render() {
-    const newStyle = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      // ':hover': {
-      //   backgroundColor: 'lightgreen',
-      //   color: 'black'
-      // }
-    };
 
     let persons = null;
 
@@ -90,19 +78,19 @@ class App extends Component {
       // }
     };
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');  // classes = ['red']
+      assignedClasses.push( classes.red );  // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold);');   // classes = ['red', 'bold']
+      assignedClasses.push( classes.bold );   // classes = ['red', 'bold']
     }
 
     return (
       //<StyleRoot>
-        <div className="App">
+        <div className={classes.App}>
           <h1>I am a React App!</h1>
-          <p className={classes.join(' ')}>This is really working!!!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!!!</p>
           {/* <button
               style={newStyle} 
               onClick={this.switchNameHandler.bind(this, 'Maximilian!!!')}>Switch Name</button> */}
